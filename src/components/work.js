8d176d5d9d6674f33f9b704d6d46b1392be0ff3d
Sapir Shanskhalil <ss3273@cornell.edu>
2021-04-26 20:43:40 +0300

    instagram albums post

--- src/components/work.js
+++ src/components/work.js
@@ -484,6 +484,324 @@ export default class Work extends Component {
             meandbaeinrome18, and so on and so forth.
             <br />
           </p>
+          <p>
+            And my first reaction to that was <br />
+            <br />
+            <i>THAT’S A GREAT IDEA! I SHOULD START DOING THAT TOO!</i> <br />
+            <br />
+            But I'm a designer-- <br />
+            So my head took me in a different direction...
+          </p>
+          <p>
+            <i>
+              PROBLEM: When I have multiple pictures from an event or an
+              experience, I want to group them together in order to either
+              remember them better or make them more easier to share (whether to
+              only share with certain people, or to not overwhelm my feed with
+              posts), but I can’t do that well because there is no feature that
+              groups my posts.
+            </i>
+          </p>
+          <h2>Brainstorming Solutions</h2>
+          <p>
+            As I tried coming up with a number of different unique solutions
+            that solved the problem, I decided that the best one was similar to
+            the current existing solution: Insta-Albums. It would provide people
+            a way to share numerous photos, however it would offer them their
+            own space-- something that a post or story cannot do.
+          </p>
+          <h3>Insta-Albums-- What Exactly Are They?</h3>
+          <p>Now to flesh out the basic idea.</p>
+          <p>
+            <em>Basic Idea: </em>
+            Some sort of collections-like feature where the user can upload
+            images just like regular Instagram posts.
+            <br />
+            Now for some important questions to flesh out this concept:
+            <ul>
+              <li>What features should these albums include?</li>
+              <li>
+                How will the user access these Insta-albums? Where should they
+                fall in the current information hierarchy?
+              </li>
+              <li>How should the page or section look like?</li>
+            </ul>
+          </p>
+          <h3>What features should these albums include?</h3>
+          <p>
+            With the problem I identified in mind (people want to post multiple
+            photos of one event grouped together), I listed all potential
+            features weighed how much they made sense and crossed off the ones
+            that did not fit. I ended up with these:
+            <ul>
+              <li>Title</li>
+              <li>Description</li>
+              <li>Collaborators (and managing them)</li>
+              <li>
+                Posts: posts that are shared in your feed, and posts that aren’t
+              </li>
+              <li>Adding posts after the fact / Removing posts</li>
+              <li>Adding multiple pictures as different posts</li>
+              <li>Followers</li>
+              <li>Managing who can see your album</li>
+              <li>Album feed</li>
+            </ul>
+            Some ideas that I ended up cutting were having a forced
+            chronological order, which I originally thought was a good idea, but
+            then realized that if someone is potentially compiling pictures
+            after a period of time, they would want to be able to order them
+            accordingly. Another one was to have the collaborators and users who
+            can see the album one and the same, which I thought wouldn’t make
+            sense for cases of people sharing an experience with people who
+            weren’t there (like a vlog or something).
+          </p>
+          <h3>
+            How will the user access these Insta-albums? Where should they fall
+            in the current information hierarchy?
+          </h3>
+          <p>
+            I drew out the information hierarchy, and given the features I just
+            laid out, and the functionality of an album, I weighed the pros and
+            cons of putting it in different positions.
+          </p>
+          <img
+            src={
+              process.env.PUBLIC_URL +
+              "post-content/instagram-info-hierarchy.jpeg"
+            }
+            alt="mypic"
+          />
+          <p>
+            There are two current post types on Instagram — the o.g. regular
+            posts and stories, which can both be accessed from the main page.
+            Post types can be accessed in two ways — viewing/editing and adding.
+            What I’m proposing is essentially a new post type, similar to what
+            regular posts are, but not quite. This is why all the nodes that
+            connected to stories were out of the question, because they’d give
+            the wrong idea of what the functionality of the new post would be.
+            Stories are more of a live update, while albums are more of a
+            compilation device.
+          </p>
+          <p>
+            <em>Viewing</em>
+          </p>
+          <p>
+            It would make sense that, like the other post types, albums were
+            given a spot on the profile page. Similarly, they should also be
+            given a platform on the main page. In the case of the accounts page,
+            there are multiple locations it can be within it — underneath or
+            over where the stories are, on the bar with the account pictures, or
+            in the hamburger nav bar in the top right corner. This is how each
+            of those would look like, roughly (new feature is shown in red):
+          </p>
+          <div class="flex-row">
+            <img
+              src={
+                process.env.PUBLIC_URL +
+                "post-content/instagram-profile-pos-1.png"
+              }
+              alt="mypic"
+            />
+            <img
+              src={
+                process.env.PUBLIC_URL +
+                "post-content/instagram-profile-pos-2.png"
+              }
+              alt="mypic"
+            />
+            <img
+              src={
+                process.env.PUBLIC_URL +
+                "post-content/instagram-profile-pos-3.png"
+              }
+              alt="mypic"
+            />
+            <img
+              src={
+                process.env.PUBLIC_URL +
+                "post-content/instagram-profile-pos-4.png"
+              }
+              alt="mypic"
+            />
+          </div>
+          <p>
+            After reviewing these wireframes, I’ve decided to go with the first
+            one — to place the new feature in the bar with account pictures.
+            Next to the stories it looks too cluttered and feels like it should
+            open up a pop-up, similar to the stories. In the hamburger nav, it
+            feels more like a personal setting, which would make it unfit to
+            potentially share, but on the bar, it feels like it should be part
+            of the sharing experience of Instagram as well as an element of the
+            account, as are the rest of the buttons in that bar.
+          </p>
+          <p>
+            In the case of the main page, there are also multiple options for
+            location — it can be on the top bar, over or under the stories bar,
+            or integrated with the main feed. It wouldn’t make sense for it to
+            be on the top bar since none of the top bar buttons (add to story,
+            tv, and DMs) have anything to do with this new feature. I sketched
+            out wireframes for the latter two and listed what each would imply.
+          </p>
+          <img
+            src={process.env.PUBLIC_URL + "post-content/instagram-main-pos.png"}
+            alt="mypic"
+          />
+          <p>
+            I ended up deciding on the second wireframe where in place of the
+            profile name would be the album name, since it makes albums similar
+            to posts as well as makes the album the focus. But that still leaves
+            the questions of where the add feature would be.
+          </p>
+          <p>
+            <em>Adding</em>
+          </p>
+          <p>
+            As I discussed earlier, since stories were out of the question, the
+            only nodes left were either conjoined with posts or their own
+            separate thing. A potential problem with the first option was that
+            it could potentially take away from the regular posts feature, as
+            each time you’d make a post, you would have to choose if it was
+            either a post or an album, however, the problem with the latter
+            option was finding a place to put it that is not too confusing. I
+            looked to Facebook albums for advice. Facebook albums have two
+            methods of adding an album, through posts (on the main page) and
+            through the albums page. Inspired by this I thought that when
+            creating a post, you should have the option to add it to an album,
+            existing or non-existing and when you open the albums page, you
+            should have the option to create an album. So in a sense, it should
+            both be joined to adding a post, but also be its own thing. I
+            sketched some wireframes of this as well to decide where in the flow
+            of creating a regular post it should go, but because I haven’t
+            designed the albums page yet, I tabled the second add feature for
+            later.
+          </p>
+          <img
+            src={
+              process.env.PUBLIC_URL +
+              "post-content/instagram-album-creation.jpeg"
+            }
+            alt="mypic"
+          />
+          <p>
+            I decided upon the third section of wireframes where the user can
+            select the album where he or she would select the location, and, in
+            addition, the user would be able to convert a multiple photos post
+            to individual album posts.
+          </p>
+          <p>
+            <em>How should the section and pages look like?</em>
+          </p>
+          <p>
+            For the section section (no pun intended), I decided to draw
+            inspiration from the existing feed, collection, and Facebook Albums.
+          </p>
+          <img
+            src={
+              process.env.PUBLIC_URL +
+              "post-content/instagram-section-inspo.png"
+            }
+            alt="mypic"
+          />
+          <p>
+            They are all similar in the sense that they use a grid format,
+            although both collections and Facebook albums use 2 columns instead
+            of the 3 column format that the feed uses. Additionally, both the
+            Facebook albums and collections have titles on their display page —
+            so it would make sense that they use 2 columns instead of 3 so that
+            the title texts don’t have to be so small. The differences between
+            the two are that the Facebook albums include the number of posts,
+            while collections omits them, the add buttons are in different
+            places, and collections has all posts, while Facebook albums don’t.
+            The solution that would make the most sense is having the
+            collections and Facebook albums grid in the layout of a user profile
+            like so:
+          </p>
+          <img
+            src={
+              process.env.PUBLIC_URL +
+              "post-content/instagram-section-wireframe.png"
+            }
+            alt="mypic"
+          />
+          <p>
+            I considered replacing the post add button with the create an album
+            button, but I thought the replacement would be too unclear and
+            confusing for users.
+          </p>
+          <p>
+            Now that we have the section laid out— what will the Insta-album
+            page itself look like? <br />
+          </p>
+          <p>
+            In order to answer this question, I first analyzed what content the
+            page will hold.
+            <ul>
+              <li>
+                The page holds the album, so the initial page must contain a
+                presentation of the album. That is the main content. (This part
+                is taken care of)
+              </li>
+              <li>
+                The albums hold the posts, so there must be some way to get to
+                them.
+              </li>
+              <li>
+                And the posts should function just as any other Instagram post.
+              </li>
+            </ul>
+            Kind of like babushka dolls:
+          </p>
+          <img
+            src={
+              process.env.PUBLIC_URL + "post-content/instagram-babushkas.png"
+            }
+            alt="mypic"
+          />
+          <p>
+            Taking all things into consideration I started brainstorming and
+            creating sketches of what potential Insta-album pages could look
+            like. I decided upon this configuration:
+          </p>
+          <img
+            src={
+              process.env.PUBLIC_URL + "post-content/instagram-album-page.png"
+            }
+            alt="mypic"
+          />
+          <p>
+            Similar to a user profile, I figured it should be a header with a
+            grid too. In addition, I thought it would be a good idea to let the
+            user be able to add a little description of the album if they wanted
+            to, similar to a profile bio. There shouldn’t be a profile picture,
+            because the album itself is filled with pictures. So really there
+            should only be a description at the top. I kept the bar with
+            different ways to display the posts — grid view, feed view, but
+            tagged wouldn’t be needed since it is an album and not a person. I
+            also kept the bottom bar since both saved and the profile had it.
+          </p>
+          <h2>Final Feature</h2>
+          <p>
+            Although the Album page itself changed a little bit from its
+            wireframe iteration, after realizing that it would fit better if it
+            looked more like something that already existed within Instagram,
+            this was the final product:
+          </p>
+          <img
+            src={
+              process.env.PUBLIC_URL +
+              "post-content/instagram-albums-final.jpeg"
+            }
+            alt="mypic"
+          />
+          <h2>Reflection</h2>
+          <p>
+            One thing I could have done to improve the solution was to conduct
+            user testing, something that I neglected due to time constraints and
+            wanting to practice iterations and wireframing. I'm overall really
+            happy with the end high-fidelity mockups. I put a lot of work into
+            them and felt that I really improved my ability to work within an
+            existing branding scheme.
+          </p>
         </div>
       ),
       category: "User Experience Design",
